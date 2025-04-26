6d0c031046c5192c37ae30fe293bee67b45721e1
= <=>
2025-04-26 22:40:48 +0700

    create a infinity scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,36 +5,77 @@ import Card from "@/component/card";
 import Navigation from "@/component/navigation";
 import CardSkeleton from "@/component/UX/cardSkleton";
 import { useWidth } from "@/context/context";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 export default function Home() {
-  const [data,setData] = useState([])
+  const [data,setData] = useState<{
+    webformatURL : string;
+    id : number;
+    user:string; 
+    username : string; 
+    likes : string;
+    views : string; 
+    downloads : string;
+    device : string, 
+    iconDevice : number,deviceGap : string
+  }[]>([])
+  const [isFetching,setIsFetching] = useState(false)
+  const lastTriggerPoint = useRef(null)
   const widthDevice = useWidth()
+  const [page,setPage] = useState(1)
+
+  const callApi = async(currentPage : number)=>{
+    if(isFetching) return;
+    setIsFetching(true)
+    try{
+      const response = await fetch(`https://pixabay.com/api/?key=49948897-656b16e360f245897e85c17ff&q=chinese+girls&image_type=photo&pretty=true&page=${currentPage}`)
+    const datas = await response.json()
+    setData((prevData)=> [...prevData, ...datas.hits])
+    setPage((prevPage)=> prevPage + 1)
+  }catch(error){
+      console.error("Error fetchin fata:",error)
+    }finally{
+      setIsFetching(false)
+    }
+  }
+  useEffect(()=>{
+    callApi(page)
+  },[])
+
 
   useEffect(()=>{
-    const callApi = async()=>{
-        const response = await fetch("https://pixabay.com/api/?key=49948897-656b16e360f245897e85c17ff&q=indonesian+girls&image_type=photo&pretty=true")
-        const datas = await response.json()
-        setData(datas.hits)
+    const observer = new IntersectionObserver(
+      (entries) =>{
+        const target = entries[0];
+        if(target.isIntersecting && !isFetching){
+          callApi(page)
+        }
+      },{
+        root : null,
+        rootMargin : '20px',
+        threshold : 0.1
+      }
+    )
+    if(lastTriggerPoint.current){
+      observer.observe(lastTriggerPoint.current)
     }
-    callApi()
-  },[])
-  if(!data){
+
+    return()=>{
+      if(lastTriggerPoint.current){
+        observer.unobserve(lastTriggerPoint.current)
+      }
+    }
+  },[callApi,isFetching,page])
+
+  if(!data.length && isFetching){
     return( 
     <>
     <Navigation/>
     <div className={`columns-2 md:columns-4 gap-4 mt-20 p-4 h-full`}>
-    <CardSkeleton/>
-    <CardSkeleton/>
-    <CardSkeleton/>
-    <CardSkeleton/>
-    <CardSkeleton/>
-    <CardSkeleton/>
-    <CardSkeleton/>
-    <CardSkeleton/>
-    <CardSkeleton/>
-    <CardSkeleton/>
-    <CardSkeleton/>
-    <CardSkeleton/>
+    {
+      Array(12).fill(null).map((_,index)=>(
+        <CardSkeleton key={index++}/>
+      ))
+    }
     </div>
     </>
     )
@@ -53,19 +94,29 @@ export default function Home() {
   <div className={`columns-2 md:columns-4 gap-4 mt-5 p-4 h-full`}>
         {data.map((item : {
           webformatURL: string;id : number,user:string, username : string, likes : string, views : string, downloads : string, device : string, iconDevice : number,deviceGap : string
-})=>{
+},index)=>{
           return(
-            <div key={item.id}>
+            <div key={index++}>
               <Card 
                   deviceGap = {widthDevice < 1024 ? "gap-4" : "gap-8"}
                   image={item.webformatURL}
                   device={widthDevice < 1024 ? "text-md" : "text-xl"}
                   username={item.user}
-                  iconDevice={widthDevice < 1024 ? 20 : 40 } likes={item.likes} views={item.views} downloads={item.downloads}
+                  iconDevice={widthDevice < 1024 ? 20 : 40 } likes={item.likes ? item.likes.toString() : "0"} views={item.views ?  item.views.toString() : "0"} downloads={item.downloads ? item.downloads.toString() : "0"}
               />
             </div>
          )
         })}
+
+        {
+          isFetching && 
+                <>
+                <CardSkeleton/>
+                <CardSkeleton/>
+                <CardSkeleton/>
+                </>
+        }
+        <div ref={lastTriggerPoint}></div>
       </div> 
   </> 
   );
--- src/component/card/index.tsx
+++ src/component/card/index.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 export default function Card({image,username,likes,views,downloads,device,iconDevice,deviceGap} : {image : string, username : string, likes : string, views : string, downloads : string, device : string, iconDevice : number, deviceGap : string}) {
   return (
     <>
-    <div className="bg-[#7BA267] p-4 mt-3 rounded break-inside-avoid">
+    <div className="bg-white p-4 mt-3 rounded break-inside-avoid">
     <img src={image} alt="jpg" className="w-full h-auto rounded" />  
-    <p className={`flex items-center ${device} gap-2 my-2 font-semibold text-white`}>
+    <p className={`flex items-center ${device} gap-2 my-2 font-semibold text-[#415936]`}>
         <img src="user (4).svg" alt="user" className="bg-[#547246] p-1 rounded-full" width={iconDevice} />
         {username}
     </p>
